a17c3dca641c8a839c1a86c229a448739622eaa5
unknown <shitalnb11@gmail.com>
2025-10-11 15:48:16 +0530

    Added app.py for Render deployment

--- app.py
+++ app.py
@@ -1,12 +1,22 @@
-from flask import Flask
+#!/usr/bin/env python
+"""Django's command-line utility for administrative tasks."""
 import os
+import sys
 
-app = Flask(__name__)
 
-@app.route('/')
-def home():
-    return "✅ Render Python App is running successfully!"
+def main():
+    """Run administrative tasks."""
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'real_estate.settings')
+    try:
+        from django.core.management import execute_from_command_line
+    except ImportError as exc:
+        raise ImportError(
+            "Couldn't import Django. Are you sure it's installed and "
+            "available on your PYTHONPATH environment variable? Did you "
+            "forget to activate a virtual environment?"
+        ) from exc
+    execute_from_command_line(sys.argv)
 
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 10000))
-    app.run(host="0.0.0.0", port=port)
+
+if __name__ == '__main__':
+    main()
