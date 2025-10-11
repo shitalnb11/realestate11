2012322d39758a6a98fc7ec0baa5569bb7144d9e
unknown <shitalnb11@gmail.com>
2025-10-11 18:03:47 +0530

    Fix: dual database config for local and render

--- real_estate/settings.py
+++ real_estate/settings.py
@@ -69,20 +69,21 @@ WSGI_APPLICATION = 'real_estate.wsgi.application'
 # -------------------------------------------------
 # DATABASE CONFIGURATION
 # -------------------------------------------------
-DATABASES = {
-    'default': dj_database_url.config(
-        default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}",
-        conn_max_age=600,
-        ssl_require=True
-    )
-}
-# DATABASES = {
-#     'default': dj_database_url.config(
-#         default=os.environ.get('DATABASE_URL'),
-#         conn_max_age=600,
-#         ssl_require=True
-#     )
-# }
+if os.environ.get('DATABASE_URL'):
+    DATABASES = {
+        'default': dj_database_url.config(
+            default=os.environ.get('DATABASE_URL'),
+            conn_max_age=600,
+            ssl_require=True
+        )
+    }
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 # -------------------------------------------------
 # PASSWORD VALIDATION
 # -------------------------------------------------
