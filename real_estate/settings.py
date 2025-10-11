1301263c6f19bb2d94215861edeefd3cced40ad7
unknown <shitalnb11@gmail.com>
2025-10-11 17:26:43 +0530

    Switch to db.sqlite3 on Render

--- real_estate/settings.py
+++ real_estate/settings.py
@@ -69,20 +69,20 @@ WSGI_APPLICATION = 'real_estate.wsgi.application'
 # -------------------------------------------------
 # DATABASE CONFIGURATION
 # -------------------------------------------------
-# DATABASES = {
-#     'default': dj_database_url.config(
-#         default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}",
-#         conn_max_age=600,
-#         ssl_require=False
-#     )
-# }
 DATABASES = {
     'default': dj_database_url.config(
-        default=os.environ.get('DATABASE_URL'),
+        default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}",
         conn_max_age=600,
         ssl_require=True
     )
 }
+# DATABASES = {
+#     'default': dj_database_url.config(
+#         default=os.environ.get('DATABASE_URL'),
+#         conn_max_age=600,
+#         ssl_require=True
+#     )
+# }
 # -------------------------------------------------
 # PASSWORD VALIDATION
 # -------------------------------------------------
