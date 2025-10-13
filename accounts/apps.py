1b55c01b166f63ac87436bb0cb64aeefca6783dd
unknown <shitalnb11@gmail.com>
2025-10-13 09:02:21 +0530

    Fix: auto create superuser moved to apps.py (after apps loaded)

--- accounts/apps.py
+++ accounts/apps.py
@@ -4,3 +4,13 @@ from django.apps import AppConfig
 class AccountsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'accounts'
+
+    def ready(self):
+        from django.contrib.auth.models import User
+        if os.environ.get('CREATE_SUPERUSER_ON_DEPLOY') == '1':
+            username = "admin"
+            email = "admin@gmail.com"
+            password = "Admin@12345"
+            if not User.objects.filter(username=username).exists():
+                User.objects.create_superuser(username=username, email=email, password=password)
+                print("✅ Superuser created automatically on deploy!")
