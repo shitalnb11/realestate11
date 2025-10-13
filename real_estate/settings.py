99560301acc55daca64d9863d31e3675659f05bf
unknown <shitalnb11@gmail.com>
2025-10-13 09:28:38 +0530

    Added createadmin management command and updated settings

--- real_estate/settings.py
+++ real_estate/settings.py
@@ -163,12 +163,3 @@ DEFAULT_FROM_EMAIL = 'Real Estate <yourcompany@gmail.com>'
 # -------------------------------------------------
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-from django.contrib.auth.models import User
-
-if os.environ.get('CREATE_SUPERUSER_ON_DEPLOY') == '1':
-    username = "admin"
-    email = "admin@gmail.com"
-    password = "Admin@12345"
-    if not User.objects.filter(username=username).exists():
-        User.objects.create_superuser(username=username, email=email, password=password)
-        print("✅ Superuser created automatically on deploy!")
